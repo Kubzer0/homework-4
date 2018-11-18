3f77bb8fcef2d4a5b8843c82acd14cc7754029a7
kubzer0 <jakub.gryka@gmail.com>
2018-11-18 18:01:51 +0100

    eventlistener added to each button on list, delete task function added for each deleteButton

--- script.js
+++ script.js
@@ -15,28 +15,40 @@ class ToDo {
 
         const ul = document.createElement('ul')
 
-        this.tasks.forEach(task => {
+        this.tasks.forEach((task, index) => {
             const li = document.createElement('li')
+            const buttonDelete = document.createElement('button')
+            buttonDelete.innerText = 'Delete task from the list'
+
+            buttonDelete.addEventListener('click', () => this.deleteTask(index))
             li.innerText = task.text
+            li.appendChild(buttonDelete)
             ul.appendChild(li)
         })
         document.body.appendChild(ul)
+
     }
     makeInterface() {
         const input = document.createElement('input')
         const buttonAdd = document.createElement('button')
-        const buttonDelete = document.createElement('button')
         buttonAdd.innerText = 'Add task to list'
-        buttonDelete.innerText = 'Delete task from the list'
+
 
         buttonAdd.addEventListener(
             'click',
             () => this.addTask(input.value)
         )
 
+
+
         document.body.appendChild(input)
         document.body.appendChild(buttonAdd)
-       
+
+    }
+
+    deleteTask(index) {
+        this.tasks = this.tasks.slice(0, index).concat(this.tasks.slice(index + 1))
+        this.render()
     }
 }
 
@@ -49,9 +61,6 @@ class Task {
 
 const toDo1 = new ToDo()
 
-toDo1.addTask('zadanie 1')
-
-toDo1.addTask('zadanie 2')
 
 console.log(toDo1)
 
