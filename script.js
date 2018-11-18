4357cab935527295528eda6d51716871f01de0a7
kubzer0 <jakub.gryka@gmail.com>
2018-11-18 16:49:47 +0100

    addTask to empty array added

--- script.js
+++ script.js
@@ -1,6 +1,15 @@
 class ToDo {
-
+constructor(){
+    this.tasks= []
+}
+addTask (text) {
+    this.tasks.push(text)
+}
 }
 
 const toDo1 = new ToDo()
+
+toDo1.addTask('zadanie 1')
+
+console.log(toDo1)
 
