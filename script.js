aa024b3ea744271b91b96b7ae7dfe02d0903b4f0
kubzer0 <jakub.gryka@gmail.com>
2018-11-18 17:25:23 +0100

    interface added, not attached delete button added

--- script.js
+++ script.js
@@ -11,22 +11,41 @@ class ToDo {
     render() {
         document.body.innerHTML = ''
 
+        this.makeInterface()
+
         const ul = document.createElement('ul')
-  
+
         this.tasks.forEach(task => {
-          const li = document.createElement('li')
-          li.innerText = task.text
-          ul.appendChild(li)
+            const li = document.createElement('li')
+            li.innerText = task.text
+            ul.appendChild(li)
         })
         document.body.appendChild(ul)
+    }
+    makeInterface() {
+        const input = document.createElement('input')
+        const buttonAdd = document.createElement('button')
+        const buttonDelete = document.createElement('button')
+        buttonAdd.innerText = 'Add task to list'
+        buttonDelete.innerText = 'Delete task from the list'
+
+        buttonAdd.addEventListener(
+            'click',
+            () => this.addTask(input.value)
+        )
+
+        document.body.appendChild(input)
+        document.body.appendChild(buttonAdd)
+       
+    }
 }
-}
+
 
 class Task {
     constructor(text) {
-      this.text = text
+        this.text = text
     }
-  }
+}
 
 const toDo1 = new ToDo()
 
