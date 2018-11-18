2f71490697ae06788151be9d74de2a2b656dc509
kubzer0 <jakub.gryka@gmail.com>
2018-11-18 17:14:46 +0100

    render function added, modified adding tasks to array (now uses separate class)

--- script.js
+++ script.js
@@ -1,15 +1,38 @@
 class ToDo {
-constructor(){
-    this.tasks= []
+    constructor() {
+        this.tasks = []
+        this.render()
+    }
+    addTask(text) {
+        this.tasks.push(new Task(text))
+        this.render()
+
+    }
+    render() {
+        document.body.innerHTML = ''
+
+        const ul = document.createElement('ul')
+  
+        this.tasks.forEach(task => {
+          const li = document.createElement('li')
+          li.innerText = task.text
+          ul.appendChild(li)
+        })
+        document.body.appendChild(ul)
 }
-addTask (text) {
-    this.tasks.push(text)
 }
-}
+
+class Task {
+    constructor(text) {
+      this.text = text
+    }
+  }
 
 const toDo1 = new ToDo()
 
 toDo1.addTask('zadanie 1')
+
+toDo1.addTask('zadanie 2')
 
 console.log(toDo1)
 
