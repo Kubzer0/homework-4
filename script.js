4d7f2464177d1d316ebe57d25d20707eaa55c683
kubzer0 <jakub.gryka@gmail.com>
2018-11-19 21:55:40 +0100

    event listner added to li elements, toogle is completed added, initial is completed value added

--- script.js
+++ script.js
@@ -21,6 +21,11 @@ class ToDo {
             buttonDelete.innerText = 'Delete task from the list'
 
             buttonDelete.addEventListener('click', () => this.deleteTask(index))
+
+            li.addEventListener('click', () => this.toggleTask(task))
+
+            this.taskIsCompletedStyle(task, li)
+
             li.innerText = task.text
             li.appendChild(buttonDelete)
             ul.appendChild(li)
@@ -50,12 +55,32 @@ class ToDo {
         this.tasks = this.tasks.slice(0, index).concat(this.tasks.slice(index + 1))
         this.render()
     }
+
+    toggleTask(task) {
+        if (task.isCompleted === true) {
+            task.isCompleted = false
+        } else {
+            task.isCompleted = true
+        }
+        this.render()
+    }
+
+    taskIsCompletedStyle(task, li) {
+        if (task.isCompleted === false) {
+            li.style.textDecoration = "none"
+        } else {
+            li.style.textDecoration = "line-through"
+        }
+    }
+
+
 }
 
 
 class Task {
     constructor(text) {
         this.text = text
+        this.isCompleted = true
     }
 }
 
